2ac1160dbff51a82f78070be8c276b30980dbc82
Roberto <rorepoid@gmail.com>
2021-01-09 23:51:39 -0500

    feat: Add users route

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,12 @@
 import { Application, Router } from "https://deno.land/x/oak/mod.ts";
+import { getAllUsers } from "./handler/getAllUsers.ts";
 
 const router = new Router();
 
 router.get('/', ctx => {
     ctx.response.body = "Hello world";
 })
+
+router.get('/users', getAllUsers)
 
 export default router;
